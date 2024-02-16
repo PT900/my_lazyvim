2812cfa22aa2889900004d13713bab0488f6b12a
Pisanu Boonyaanan <peat2512@hotmail.com>
2024-02-16 22:35:32 +0700

    Some custom config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- Remove comment on newlining (currently not working)
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = augroup("new_format"),
+  callback = function()
+    vim.opt.formatoptions = vim.opt.formatoptions - { "c", "r", "o" }
+  end,
+})
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+keymap.set("n", "x", '"_x') -- Delete single character without saving to clipboard
+keymap.set("n", "<C-a>", "gg<S-v>G") -- Select all
+keymap.set("n", "+", "<C-a>") -- Increment
+keymap.set("n", "-", "<C-x>") -- Decrement
+keymap.set("i", "<C-BS>", 'vb"_d', opts) -- Delete a word backwards (not worked)
+keymap.set("n", "<C-s>", "<Cmd>w<CR>") -- Save with Ctrl+s
+keymap.set("n", "<C-c>", "Y") -- Yank with Ctrl+c in normal mode
+keymap.set("v", "<C-c>", "Y") -- Yank with Ctrl+c in visual mode
