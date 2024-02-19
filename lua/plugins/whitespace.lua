0a68223016188bed20b41d50a6cf2fb62e54c82a
Pisanu Boonyaanan <peat2512@hotmail.com>
2024-02-19 18:50:00 +0700

    Remove save from trimming whitespace

--- lua/plugins/whitespace.lua
+++ lua/plugins/whitespace.lua
@@ -2,5 +2,5 @@ return {
   "jdhao/whitespace.nvim",
   event = "VimEnter",
 
-  vim.keymap.set("n", "<S-F12>", "<Cmd>StripTrailingWhitespace<CR><Cmd>w<CR><ESC>", { noremap = true, silent = true }),
+  vim.keymap.set("n", "<S-F12>", "<Cmd>StripTrailingWhitespace<CR>", { noremap = true, silent = true }),
 }
